d1b8ced6e46b6c651ba59b3d7f2c9f3de6c03295
maleehazulifqar <maleehazulifqar777786@gmail.com>
2026-03-19 11:40:39 -0700

    Fix: host 0.0.0.0 for Railway

--- Server.py
+++ Server.py
@@ -83,5 +83,5 @@ def get_files():
     return jsonify({'files': uploaded_files})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
